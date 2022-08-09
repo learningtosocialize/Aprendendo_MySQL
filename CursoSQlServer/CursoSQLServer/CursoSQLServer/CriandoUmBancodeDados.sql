a65ad5420a211cee3cf92712d1ccba0548536371
Leonardo <89586048+learningtosocialize@users.noreply.github.com>
2022-08-09 17:51:21 -0300

    Add files via upload

---
+++ CursoSQlServer/CursoSQLServer/CursoSQLServer/CriandoUmBancodeDados.sql
@@ -0,0 +1,17 @@
+/*
+	Tudo que estiver aqui dentro é um comentário;
+	Criando um banco de dados
+	Autor: Leonardo Auco Brochetti
+	Data: 15/04/2022 (data sendo: data do inicio do projeto, ou, data da ultima manutenção)
+*/
+
+
+-- Aqui é um comentário de apenas uma linha;
+
+
+-- Criando um banco via T-SQL
+CREATE DATABASE DepartamentoPessoal;
+
+
+-- Colocando um Banco em Uso
+USE DepartamentoPessoal;
